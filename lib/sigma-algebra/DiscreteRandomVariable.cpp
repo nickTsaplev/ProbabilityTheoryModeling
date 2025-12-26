47f5f8a6f1561b1a22211e1f8eb766cd0d06fdb7
Michael Kulikov <lego-mike@mail.ru>
2025-12-26 13:29:32 +0300

    style: change spaces from 4 to 2

--- lib/sigma-algebra/DiscreteRandomVariable.cpp
+++ lib/sigma-algebra/DiscreteRandomVariable.cpp
@@ -3,32 +3,31 @@
 #include <stdexcept>
 
 namespace ptm {
-    DiscreteRandomVariable::DiscreteRandomVariable(const OutcomeSpace& omega, const ProbabilityMeasure& P, std::vector<double> values) 
-        : omega_(omega)
-        , P_(P)
-        , values_(std::move(values))
-    {
-        if (values_.size() != omega_.GetSize())
-            throw std::invalid_argument("Number of values must match number of outcomes");
+DiscreteRandomVariable::DiscreteRandomVariable(const OutcomeSpace& omega,
+                                               const ProbabilityMeasure& P,
+                                               std::vector<double> values) :
+    omega_(omega), P_(P), values_(std::move(values)) {
+  if (values_.size() != omega_.GetSize())
+    throw std::invalid_argument("Number of values must match number of outcomes");
 
-        if (!P_.IsValid(1e-10))
-            throw std::invalid_argument("Probability must be valid with sum = 1");
-    }
-
-    std::optional<double> DiscreteRandomVariable::Value(OutcomeSpace::OutcomeId id) const {
-        if (id >= values_.size())
-            return std::nullopt;
-
-        return values_[id];
-    }
-
-    double DiscreteRandomVariable::ExpectedValue() const {
-        double result = 0.0;
-
-        for (size_t i = 0; i < values_.size(); ++i) {
-            result += values_[i] * P_.GetAtomicProbability(i);
-        }
-
-        return result;
-    }
+  if (!P_.IsValid(1e-10))
+    throw std::invalid_argument("Probability must be valid with sum = 1");
 }
+
+std::optional<double> DiscreteRandomVariable::Value(OutcomeSpace::OutcomeId id) const {
+  if (id >= values_.size())
+    return std::nullopt;
+
+  return values_[id];
+}
+
+double DiscreteRandomVariable::ExpectedValue() const {
+  double result = 0.0;
+
+  for (size_t i = 0; i < values_.size(); ++i) {
+    result += values_[i] * P_.GetAtomicProbability(i);
+  }
+
+  return result;
+}
+} // namespace ptm
--- lib/sigma-algebra/ProbabilityMeasure.cpp
+++ lib/sigma-algebra/ProbabilityMeasure.cpp
@@ -1,48 +1,46 @@
 #include "ProbabilityMeasure.hpp"
 
-#include <stdexcept>
 #include <numeric>
+#include <stdexcept>
 
 namespace ptm {
-    ProbabilityMeasure::ProbabilityMeasure(const OutcomeSpace& omega)
-        : omega_(omega)
-        , atom_probs_(omega.GetSize(), 0.0) 
-    {}
-
-    void ProbabilityMeasure::SetAtomicProbability(OutcomeSpace::OutcomeId id, double p) {
-        if (id >= atom_probs_.size())
-            throw std::out_of_range("Id out of range");
-
-        if (p > 1.0 || p < 0.0)
-            throw std::invalid_argument("Invalid probability");
-
-        atom_probs_[id] = p;
-    }
-
-    double ProbabilityMeasure::GetAtomicProbability(OutcomeSpace::OutcomeId id) const {
-        if (id >= atom_probs_.size())
-            throw std::out_of_range("Id out of range");
-
-        return atom_probs_[id];
-    }
-
-    bool ProbabilityMeasure::IsValid(double eps) const {
-        double sum = std::accumulate(atom_probs_.begin(), atom_probs_.end(), 0.0);
-        return std::abs(sum - 1.0) < std::abs(eps);
-    }
-
-    double ProbabilityMeasure::Probability(const Event& event) const {
-        if (event.GetSize() != omega_.GetSize())
-            throw std::out_of_range("Different sizes");
-
-        double result = 0.0;
-
-        for (size_t i = 0; i < atom_probs_.size(); ++i) {
-            if (event.Contains(i)) {
-                result += atom_probs_[i];
-            }
-        }
-    
-        return result;
-    }
+ProbabilityMeasure::ProbabilityMeasure(const OutcomeSpace& omega) : omega_(omega), atom_probs_(omega.GetSize(), 0.0) {
 }
+
+void ProbabilityMeasure::SetAtomicProbability(OutcomeSpace::OutcomeId id, double p) {
+  if (id >= atom_probs_.size())
+    throw std::out_of_range("Id out of range");
+
+  if (p > 1.0 || p < 0.0)
+    throw std::invalid_argument("Invalid probability");
+
+  atom_probs_[id] = p;
+}
+
+double ProbabilityMeasure::GetAtomicProbability(OutcomeSpace::OutcomeId id) const {
+  if (id >= atom_probs_.size())
+    throw std::out_of_range("Id out of range");
+
+  return atom_probs_[id];
+}
+
+bool ProbabilityMeasure::IsValid(double eps) const {
+  double sum = std::accumulate(atom_probs_.begin(), atom_probs_.end(), 0.0);
+  return std::abs(sum - 1.0) < std::abs(eps);
+}
+
+double ProbabilityMeasure::Probability(const Event& event) const {
+  if (event.GetSize() != omega_.GetSize())
+    throw std::out_of_range("Different sizes");
+
+  double result = 0.0;
+
+  for (size_t i = 0; i < atom_probs_.size(); ++i) {
+    if (event.Contains(i)) {
+      result += atom_probs_[i];
+    }
+  }
+
+  return result;
+}
+} // namespace ptm
